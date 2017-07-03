29a04cc65dd5e1e4e3bc825761f940cbc60cfdd0
Casey Ledbetter <caseyledbetter@Caseys-MacBook-Pro.local>
2017-07-03 10:47:42 -0700

    commit before pushing to heroku

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const Twilio = require('twilio');
+const myPhoneNumber = '+18586106178';
+const accountSid = 'AC3298223b18c68c680de310c11053895b';
+const authToken = '4e58511711196a1dce293db1f618992f';
+var twilioClient = new Twilio(accountSid, authToken);
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+app.post('/sendMessage', (req, res) => {
+    const message = `Hello there you handsome man!`
+
+    twilioClient
+        .messages
+        .create({
+            body: message,
+            to: myPhoneNumber, 
+            from: '+18582511072' 
+        })
+        .then(function(message) {
+            res.send('ok');
+        });
+});
+
+app.post('/twilioReply', (req, res) => {
+    twilioClient
+        .messages
+        .create({
+            body: 'Got Reply',
+            to: myPhoneNumber, 
+            from: '+18582511072' 
+        })
+        .then(function(message) {
+            res.send('ok');
+        });
+});
+
+app.listen(port, function() {
+    console.log(`Server listening on port ${port}`);
+});
